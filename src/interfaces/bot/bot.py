202579fd425129f47ca15baa8d6b9862ecad44a1
kiselev-kirill <kiselev99kirill1501@gmail.com>
2026-04-19 20:19:55 +0300

    apply fix in bot.py to handle en

--- src/interfaces/bot/bot.py
+++ src/interfaces/bot/bot.py
@@ -6,7 +6,8 @@ from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.types import BotCommand, BotCommandScopeDefault
 
 from src.config import settings
-from src.interfaces.bot.constants import BOT_COMMANDS
+from src.interfaces.bot.constants import get_bot_commands
+from src.interfaces.bot.i18n import DEFAULT_LOCALE, SUPPORTED_LOCALES
 
 from src.interfaces.bot.handlers.chat import router as chat_router
 from src.interfaces.bot.middleware.action import ChatActionMiddleware
@@ -14,11 +15,25 @@ from src.interfaces.bot.middleware.throttling import ThrottlingMiddleware
 
 
 async def _register_bot_commands(bot: Bot) -> None:
-    commands = [
+    default_commands = [
         BotCommand(command=command, description=description)
-        for command, description in BOT_COMMANDS
+        for command, description in get_bot_commands(DEFAULT_LOCALE)
     ]
-    await bot.set_my_commands(commands=commands, scope=BotCommandScopeDefault())
+    await bot.set_my_commands(
+        commands=default_commands,
+        scope=BotCommandScopeDefault(),
+    )
+
+    for locale in sorted(SUPPORTED_LOCALES):
+        commands = [
+            BotCommand(command=command, description=description)
+            for command, description in get_bot_commands(locale)
+        ]
+        await bot.set_my_commands(
+            commands=commands,
+            scope=BotCommandScopeDefault(),
+            language_code=locale,
+        )
 
 
 async def main():
